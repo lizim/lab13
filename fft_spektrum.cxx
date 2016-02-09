4dab376a4bb013eaf829ba9bb4aca6c72900b922
Sonja Allani <sallani@pc01.pool.tp1.uni-duesseldorf.de>
2016-02-09 11:42:01 +0100

    lab13

--- fft_spektrum.cxx
+++ fft_spektrum.cxx
@@ -22,24 +22,32 @@ int main(int argc, char** argv){
 	char *in_file  = argv[1];
 	char *out_file = argv[2];
 
-	const int N = 16384;
-	double L;
+	const int N = 16384; // Anzahl Punkte aus Datei. Könnte man theoretisch auch auslesen 
+	double L; // Länge des Intervalls in x 
+	
 
 	// Allocate memory
-	fftw_complex* f = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * (N/2+1));
-	double* inR  = (double*) malloc(sizeof(double)*N);
+	fftw_complex* f = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * (N/2+1)); //Hier soll Fouriertrafo rein
+	double* inR  = (double*) malloc(sizeof(double)*N); // hier sollen Daten aus Datei rein
 
-  // Create plan
-	fftw_plan FW  = fftw_plan_dft_r2c_1d(N, inR, f, FFTW_ESTIMATE);
+  // Create PLAN
+	fftw_plan FW  = fftw_plan_dft_r2c_1d(N, inR, f, FFTW_ESTIMATE); // Discrete Fourier Trafo Real 2 Complex 1Dim, estimate--> sucht sich selbst aus welche Mthode 
 
 	// Read input data
 
 	// Call function which reads the data from
 	// the input file into the array inR
 
+	ifstream in(in_file);
+	for(int i=0;i<N;i++){
+	
+	  in >> L; // am Ende nur L gespeichert, aber nur wenn x0=0
+	  in >> inR[i];
+	}
+	in.close();
 
   // Calculate FFT
-  fftw_execute(FW);
+  fftw_execute(FW); // nutz bPlan von oben
 
   // write output file
   writeData(f, N,  L, out_file);
@@ -58,7 +66,7 @@ void writeData(const fftw_complex* const f, const int N, const double L,const ch
 	double pk;
 
 	for(int i=0; i<=N/2; i++){
-		pk = sqrt(f[i][0]*f[i][0] + f[i][1]*f[i][1])/N;
+		pk = sqrt(f[i][0]*f[i][0] + f[i][1]*f[i][1])/N; //f[i][0] = realteil f[i][1] = imaginärteil 
 		out << i*dk << "\t" << pk << "\t" << f[i][0] << "\t" << f[i][1] << endl;
 	}
 
